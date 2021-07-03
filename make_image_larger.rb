550164c4c4022401e5afc1200222ad7864413ec5
申思维 <shensiwei@siwei.tech>
2021-07-03 08:36:04 +0800

    修复了发起90次之后就会产生400错误

--- make_image_larger.rb
+++ make_image_larger.rb
@@ -7,53 +7,51 @@ require './lib/logger_tool.rb'
 '''
 让图片无损放大
 '''
+# 可以使用一个月
+access_token = '24.08bbec0d2a207e6b0eb513b767bc0270.2592000.1627810765.282335-'
 
-access_token = '24.5499bd61ab43a9784f9cdb9d392b038d.2592000.1627798482.282335-24469596'
+request_url = "https://aip.baidubce.com/rest/2.0/image-process/v1/image_quality_enhance?access_token=#{access_token}"
 
-request_url = "https://aip.baidubce.com/rest/2.0/image-process/v1/image_quality_enhance"
+# 这个很重要
+@file_prefix = 'vcd3_'
+@file_postfix = '.png'
 
 origin_image_folder = 'tmp_origin_image_folder'
 enlarged_image_folder = 'tmp_enlarged_image_folder'
 
-@@logger = LoggerTool.get_logger
+@logger = LoggerTool.get_logger
 
 # 二进制方式打开图片文件
 Dir["#{origin_image_folder}/*.png"].sort.each do |origin_png|
 
   base_file_name = origin_png.gsub(origin_image_folder + '/', '')
-  index = base_file_name.gsub('vcd3-','').gsub('.png', '').to_i
-  if index < 300
-    @@logger.info "== index is: #{index}, <= 300, skip"
+  index = base_file_name.gsub(@file_prefix,'').gsub(@file_postfix, '').to_i
+  if index < 566
+    @logger.info "== index is: #{index}, <= 477, skip"
     next
   end
 
-  f = File.read(origin_png)
-  img = Base64.encode64(f)
-
-  params = {"image":img}
-  request_url = request_url + "?access_token=" + access_token
-  headers = {'content-type': 'application/x-www-form-urlencoded'}
-
   options = {
-    body: { "image": img},
-    headers: headers,
+    body: {"image":Base64.encode64(File.read(origin_png))},
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
   }
 
   response = HTTParty.post(request_url, options)
-
   new_file_name = "#{enlarged_image_folder}/#{base_file_name}"
+  puts "== request_url: #{request_url}, new_file_name: #{new_file_name}"
 
-  @@logger.info "== processing : #{new_file_name}"
-  @@logger.info response.code
-  @@logger.info response.body if response.code >= 300
+  @logger.info "== processing : #{new_file_name}"
+  @logger.info response.code
+  @logger.info response.body if response.code >= 300
 
   begin
     File.open(new_file_name, 'wb') do |f|
       f.write(Base64.decode64(JSON.parse(response.body)['image']))
     end
   rescue Exception => e
-    @@logger.error e
-    @@logger.error response.body
+    @logger.error e
+    @logger.error "== error, index: #{index}"
+    @logger.error response
   end
 
   sleep 0.1
