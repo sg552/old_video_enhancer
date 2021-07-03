e781b8fc30335993d8296cf809fa2ce4d32cc753
申思维 <shensiwei@siwei.tech>
2021-07-03 09:47:17 +0800

    extract method for enlarge image

--- make_image_larger.rb
+++ make_image_larger.rb
@@ -3,6 +3,9 @@ require "base64"
 require 'httparty'
 require 'json'
 require './lib/logger_tool.rb'
+require 'config'
+
+Config.load_and_set_settings(Config.setting_files("./config/settings.yml", "development"))
 
 '''
 让图片无损放大
@@ -10,49 +13,70 @@ require './lib/logger_tool.rb'
 # 可以使用一个月
 access_token = '24.08bbec0d2a207e6b0eb513b767bc0270.2592000.1627810765.282335-'
 
-request_url = "https://aip.baidubce.com/rest/2.0/image-process/v1/image_quality_enhance?access_token=#{access_token}"
+@request_url = "https://aip.baidubce.com/rest/2.0/image-process/v1/image_quality_enhance?access_token=#{access_token}"
 
 # 这个很重要
-@file_prefix = 'vcd3_'
-@file_postfix = '.png'
-
-origin_image_folder = 'tmp_origin_image_folder'
-enlarged_image_folder = 'tmp_enlarged_image_folder'
 
 @logger = LoggerTool.get_logger
 
-# 二进制方式打开图片文件
-Dir["#{origin_image_folder}/*.png"].sort.each do |origin_png|
+def get_index_from_file_name options
+  @logger.info "== in get_index_from_file_name"
+  @logger.info "== options: #{options.inspect}"
+  index = options[:base_file_name].gsub(options[:file_prefix],'').gsub(options[:file_postfix], '').to_i
+  return index
+end
 
-  base_file_name = origin_png.gsub(origin_image_folder + '/', '')
-  index = base_file_name.gsub(@file_prefix,'').gsub(@file_postfix, '').to_i
-  if index < 566
-    @logger.info "== index is: #{index}, <= 477, skip"
-    next
-  end
+def run options
+  origin_png = options[:origin_png]
+  base_file_name = options[:base_file_name]
 
-  options = {
+  http_options = {
     body: {"image":Base64.encode64(File.read(origin_png))},
     headers: {'content-type': 'application/x-www-form-urlencoded'},
   }
 
-  response = HTTParty.post(request_url, options)
-  new_file_name = "#{enlarged_image_folder}/#{base_file_name}"
-  puts "== request_url: #{request_url}, new_file_name: #{new_file_name}"
+  response = HTTParty.post(@request_url, http_options)
+  new_file_name = "#{options[:enlarged_image_folder]}/#{base_file_name}"
 
   @logger.info "== processing : #{new_file_name}"
   @logger.info response.code
   @logger.info response.body if response.code >= 300
 
   begin
-    File.open(new_file_name, 'wb') do |f|
+    File.open(new_file_name, 'wb') { |f|
       f.write(Base64.decode64(JSON.parse(response.body)['image']))
-    end
+    }
   rescue Exception => e
     @logger.error e
     @logger.error "== error, index: #{index}"
     @logger.error response
   end
+end
+
+# 二进制方式打开图片文件
+ORIGIN_IMAGE_FOLDER = 'tmp_origin_image_folder'
+IMAGE_TYPE = 'png'
+FROM_INDEX = 1200
+TO_INDEX = 1250
+FILE_PREFIX = "vcd3_"
+
+Dir["#{ORIGIN_IMAGE_FOLDER}/*.#{IMAGE_TYPE}"].sort.each do |origin_png|
+  base_file_name = origin_png.gsub(ORIGIN_IMAGE_FOLDER + '/', '')
+  index = get_index_from_file_name base_file_name: base_file_name, file_prefix: FILE_PREFIX, file_postfix: ".#{IMAGE_TYPE}"
+  if index < FROM_INDEX
+    @logger.info "== index is: #{index}, < #{FROM_INDEX} , skip"
+    next
+  end
+
+  if index > TO_INDEX
+    @logger.info "== index is: #{index}, > #{TO_INDEX} , return "
+    return
+  end
+
+  run origin_png: origin_png,
+    base_file_name: base_file_name,
+    origin_image_folder: ORIGIN_IMAGE_FOLDER,
+    enlarged_image_folder: 'tmp_enlarged_image_folder'
 
   sleep 0.1
 end
